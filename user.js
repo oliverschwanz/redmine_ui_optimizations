e79dea1c61ff7c13b98a288670d4bd34e14d4646
oliverschwanz <github@oliverschwanz.de>
2015-12-08 12:13:49 +0100

    Renamed

--- user.js
+++ user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         Onlinekauf Redmine UI Opt
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
